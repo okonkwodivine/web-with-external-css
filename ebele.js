7a0b2a6cb11c9093ad900ede7d5be7e00bc3e7e9
okonkwodivine <166825706+okonkwodivine@users.noreply.github.com>
2024-08-10 09:26:36 +0100

    Add files via upload

---
+++ ebele.js
@@ -0,0 +1,46 @@
+let length = 16;
+let weight = 7.5;
+console.log(length*weight)
+
+
+let color = "Yellow ";
+let lastName = "Johnson ";
+let a = "likes ";
+let b = "color";
+console.log(lastName+a+color+b)
+
+
+a =20
+b= 12
+c= 7
+d = 15
+console.log (a+b)
+console.log(c*a)
+
+
+f = 14
+d= 114
+r= 123
+n=12
+console.log(f+d+r*n)
+console.log(r+f)
+console.log(f*n)
+
+
+
+f=144
+d=12
+r=13
+n=169
+console.log(n/r+f/d)
+
+f= jonathan
+d=is
+r=very
+n=Good
+console.log(f+d+r+n)
+
+
+
+
+
